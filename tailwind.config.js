36ad6c59fdc2d8f352fcbdda359dd2466e0815f0
naik-shradha <shradhasaburi1702@gmail.com>
2024-08-11 17:38:45 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: "'Playfair Dispplay', serif",
+        lato: "'Lato', sans-serif" ,
+      }
+    },
+  },
+  plugins: [],
+}
+
